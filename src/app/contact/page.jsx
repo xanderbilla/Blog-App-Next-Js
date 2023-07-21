16220158356ba8da2a2b2e98499b1bb8f7ab9c5c
Vikas Singh <xanderbilla@gmail.com>
2023-07-21 10:31:18 +0300

    Bug1# Fixed
    Error: `'` can be escaped with `&apos;`, `&lsquo;`, `&#39;`, `&rsquo;`.  react/no-unescaped-entities                   info  - Need to disable some ESLint rules

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -6,7 +6,7 @@ import Image from 'next/image'
 const Contact = () => {
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>Let's Keep In Touch</h1>
+      <h1 className={styles.title}>Let&apos;s Keep In Touch</h1>
       <div className={styles.content}>
         <div className={styles.imageContainer}>
           <Image src='/contact.png' alt='contact' fill={true} className={styles.img}/>
